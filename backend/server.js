3efe30491b892d01b0e63112c2d1c6e7434fb81a
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2025-09-14 19:13:40 +0530

    update .env

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,12 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+});
+
 // Serve static files (uploads)
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
