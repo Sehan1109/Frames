562fb4531cc125e84c9dbcedb5b8a4260968569a
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2025-09-15 17:17:34 +0530

    fix images error

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,14 @@ import authRoutes from "./routes/authRoutes.js";
 import itemRoutes from "./routes/itemRoutes.js";
 import paymentRoutes from "./routes/paymentRoutes.js";
 import path from "path"
+import { fileURLToPath } from "url";
 
 connectDB();
 
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // ✅ Allow frontend (Netlify) to access backend
 app.use(cors({
@@ -31,7 +35,7 @@ app.get("/", (req, res) => {
     res.send("Backend running on Render 🚀");
 });
 
-app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Start server
 const PORT = process.env.PORT || 5000;
