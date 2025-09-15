1afbb286a612a49053c0a86f8b37672feaad804e
Sehan1109 <2022t01533@stu.cmb.ac.lk>
2025-09-15 13:46:57 +0530

    fix images errors

--- backend/routes/itemRoutes.js
+++ backend/routes/itemRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // ================= Multer Storage =================
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/"); // save to /uploads
+        cb(null, "uploads"); // save to /uploads
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
